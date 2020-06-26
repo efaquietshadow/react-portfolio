cd428eceba2c6b5ece678d80ed7aedae1a4a472a
kaylynfletcher <efaquietshadow@gmail.com>
2020-06-25 19:33:45 -0600

    another days progress

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import axios from 'axios'
 
 import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list"
+import PortfolioForm from "../portfolio/portfolio-form"
 
 export default class PortfolioManager    extends Component {
     constructor() {
@@ -10,10 +11,22 @@ export default class PortfolioManager    extends Component {
         this.state={
             portfolioItems: []
         }
+        this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this)
+        this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this)
+    }
+
+    handleSuccessfulFormSubmission(portfolioItem) {
+        this.setState({
+            portfolioItems: [portfolioItem].concat(this.state.portfolioItems)
+        })
+    }
+
+    handleFormSubmissionError(error) {
+        console.log("handleFormSubmissionError error", error)
     }
 
     getPortfolioItems() {
-        axios.get("https://kaylynfletcher.devcamp.space/portfolio/portfolio_items", {withCredentials: true})
+        axios.get("https://kaylynfletcher.devcamp.space/portfolio/portfolio_items?order_by=created_at&direction=desc", {withCredentials: true})
         .then(response => {
             this.setState({
                 portfolioItems: [...response.data.portfolio_items]
@@ -31,7 +44,10 @@ export default class PortfolioManager    extends Component {
         return (
             <div className='portfolio-manager-wrapper'>
                 <div className="left-column">
-                    <h1>Portfolio form..r</h1>
+                    <PortfolioForm 
+                    handleSuccessfulFormSubmission={this.handleSuccessfulFormSubmission}
+                    handleFormSubmissionError={this.handleFormSubmissionError}
+                    />
                 </div>
                 
                 <div className="right-column">
